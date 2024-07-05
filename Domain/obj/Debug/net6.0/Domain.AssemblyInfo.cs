c61c71d412ab32bdb1eade771f5fe029733c64d9
tarah.sina <tarah.sina@gmail.com>
2024-07-05 20:39:35 +0330

    Update Profile Service & Complete UserStatus

--- Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa06049f6719bde2d6afd1d7b33cf864aa3c057a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf247dda54b31c0d49aecda9860c9946d71f620c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa06049f6719bde2d6afd1d7b33cf864aa3c057a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf247dda54b31c0d49aecda9860c9946d71f620c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
